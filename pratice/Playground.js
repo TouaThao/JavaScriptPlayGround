61591a237e8a735377a96a62d8298ba4d3d7553e
Toua Thao <touathao09@gmail.com>
2018-10-04 17:31:34 -0500

    playground

---
+++ pratice/Playground.js
@@ -0,0 +1,13 @@
+// This is to pratice and the basic
+
+//Basic console.log
+//This is useful when you need to debug and follow your type of data and what being pass
+console.log('Hello World')
+
+//Basic adding and declaring or giving a vaule to an variable 
+//variable is just a fancy way of saying Hey hold this value,data,etc
+//after we assign them a value we add them up and check the value with console.log
+let num1 = 2//kitty foo
+let num2 = 3//kitty foo
+let total = num1+num2
+console.log(total)
