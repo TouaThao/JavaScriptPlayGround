15de9332b09f7acd5c5a64cd96da5ac61aff4569
Toua Thao <touathao09@gmail.com>
2018-10-04 18:04:14 -0500

    added Variable.js

---
+++ pratice/Basic.js
@@ -0,0 +1,46 @@
+// This is to pratice and the basic
+
+//Basic console.log
+//This is useful when you need to debug and follow your type of data and what being pass
+console.log('Hello World')
+
+//Basic adding and declaring or giving a vaule to an variable 
+//variable is just a fancy way of saying Hey hold this value,data,etc
+//after we assign them a value we add them up and check the value with console.log
+let num1 = 2//kitty foo
+let num2 = 3//kitty foo
+let total = num1+num2
+console.log(total)
+
+//Method
+//.toUpperCase
+//This is one of the many Method that is build in JavaScript
+//toUpperCase() will change your string all to Upper case
+console.log('The Cat is hungry'.toUpperCase())
+
+//Method
+//.toLowerCase
+//Same thing as toUpperCase but make them lower case
+console.log('THE CAT IS HUNGRY'.toLowerCase())
+
+//Method
+//.toLocaleUpperCase and toLocaleLowerCase are the same but best use on base on the langague on the browser
+//So use this would be a better method than the previous one
+console.log('The cat is thirsty'.toLocaleUpperCase())
+console.log('THE CAT IS LAZY'.toLocaleLowerCase())
+
+//Method
+//trim()
+//this is use to remove the empty space in around the string
+console.log('       The Cat is Long      '.trim())
+
+//Built in object
+//we're going use Math.random() and Math.floor
+//This will give us a random number then mutiply it by 25
+//Math.floor will make it a whole number. 
+//Just image you flooring the accelator, you're basically pushing/squshing the number to a whole number
+console.log(Math.random()* 25)
+console.log(Math.floor(Math.random()*25))
+
+// more built in object https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
