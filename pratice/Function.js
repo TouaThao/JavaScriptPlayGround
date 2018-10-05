4dc18b5b1b2be1c9a725bae48a5e9acead394444
Toua Thao <touathao09@gmail.com>
2018-10-05 14:11:09 -0500

    added for loop

---
+++ pratice/Function.js
@@ -0,0 +1,25 @@
+//Image a function as a factory. Inside the factory you could tell what logic to run
+// After the factory is built you need to invoke it to make it run.
+
+//Here we create a variable call sum and hold a value
+let sum = total(2,5);
+//next we need to build our factory
+
+function total (a,b) {
+    return a+b;
+}
+//After the factory is built we need to invoke it
+//a way to see the result is console.log
+console.log('see what we get back',sum)
+
+//for loop example
+// a for loop is use to go over all the data that inside a variable.
+// We variable that hold an array of data, in this case car maker
+let arrayItem = ['subaru','honda','gm','kia','bmw']
+
+//After we got this we need to do a for loop
+for (let index = 0; index < arrayItem.length; index++) {
+  console.log(arrayItem)
+}
+
+//If statement
---
+++ pratice/Variable.js
@@ -0,0 +1,28 @@
+//Basic adding and declaring or giving a vaule to an variable 
+//variable is just a fancy way of saying Hey hold this value,data type
+//after we assign them a value we add them up and check the value with console.log
+
+// Variable could hold Int, String, boolean , object and array
+//Variable operator could do addition, Subtraction , Multiplication , Division, Modulus (Division Remainder)
+// Increment and Decrement
+let num1 = 2//kitty foo
+let num2 = 3//kitty foo
+let total = num1+num2
+let subtotal = num1 - num2
+let mutiTotal = num1 * num2
+let divTotal = num1 / num2
+let modTotal = num1 % num2
+let incrementTotal = 5;
+incrementTotal ++
+let decreaseTotal = 6;
+decreaseTotal --
+
+console.log(total)
+console.log(subtotal)
+console.log(mutiTotal)
+console.log(divTotal)
+console.log(modTotal)
+console.log(incrementTotal)
+console.log(decreaseTotal)
+
+
