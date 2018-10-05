912beb2f329cbc815a04c3b4c66ddc494d4a8493
Toua Thao <touathao09@gmail.com>
2018-10-05 16:36:26 -0500

    one more example

--- pratice/Function.js
+++ pratice/Function.js
@@ -1,4 +1,4 @@
-//Image a function as a factory. Inside the factory you could tell what logic to run
+//Imagine a function as a factory. Inside the factory you could tell what logic to run
 // After the factory is built you need to invoke it to make it run.
 
 //Here we create a variable call sum and hold a value
@@ -11,6 +11,20 @@ function total (a,b) {
 //After the factory is built we need to invoke it
 //a way to see the result is console.log
 console.log('see what we get back',sum)
+
+//More example
+//Now let build our factory to do the logic 
+function totalSum (sum2,sum3){
+    return sum2 - sum3;
+}
+//now let check the result
+let result = totalSum(2,4)
+console.log('checking result', result)
+// so step by step
+// we created a factory that take in sum2 and sum3. sum2/sum3 could be anything
+// inside the factory we give it the logic to run. ie we did a simple subtraction
+// after that we want to see the result so we create an variable that run/invoke the factory
+// we also give it a vaule to totalSum so it know what to take in
 
 //for loop example
 // a for loop is use to go over all the data that inside a variable.
