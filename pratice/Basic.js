2cae55c241df4fb9e7491738e373cf447825a463
Toua Thao <touathao09@gmail.com>
2018-10-10 15:24:49 -0500

    added more console

--- pratice/Basic.js
+++ pratice/Basic.js
@@ -14,7 +14,16 @@ console.log(Bobby);
 //Good pratice
 //This will us on the browser to see what each object hold
 console.log({Bob,Harry,Bobby});
-
+//Here we adding color to console.log so it will be easier to see on the browser
+console.log('%c My Friend', 'color:orange;')
+console.log({Bob,Harry,Bobby})
+//Here we're going use table
+console.table([Bob,Harry,Bobby])
+//Next we could benchMark time as well
+console.time('loop')
+let i = 0;
+while (i< 50000) {i++}
+console.timeEnd('loop')
 
 //There is also so many different type of console out there
 
