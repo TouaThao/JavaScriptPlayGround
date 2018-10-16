c48ed521c8e792efc67246974bb39abbaef37a9f
Toua Thao <touathao09@gmail.com>
2018-10-16 13:17:52 -0500

    added first whiteboard question

--- pratice/WhiteBoard.js
+++ pratice/WhiteBoard.js
@@ -1,2 +1,12 @@
 //In here we will add some whiteboard question and see if we could solve them 
-// also what the logic
+// also what the logic use in the whiteboard
+
+//We want to return amy not arnold what is wrong with this JS?
+function greet (person) {
+    if (person == { name: 'amy' }) {
+      return 'hey amy'
+    } else {
+      return 'hey arnold'
+    }
+  }
+  greet({ name: 'amy' })
