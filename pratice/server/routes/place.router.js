2380100ac615b11e2a5a882572fd02fba4d03eba
Toua Thao <touathao09@gmail.com>
2018-10-19 12:58:36 -0500

    move file

---
+++ pratice/server/routes/place.router.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/address', (req, res) => {
+    const queryText =`SELECT id,name,address,city,longitude,latitude,vendor from place;`
+    pool.query(queryText)
+    .then((results) => {
+        res.send(results.rows)
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+});
+
+router.get('/store/:id', (req, res) => {
+    const queryText =`SELECT * from place where user_id = ${req.params.id};`
+    pool.query(queryText)
+    .then((results) => {
+        res.send(results.rows)
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+});
+
+
+router.get('/feedback/:id', (req,res)=>{
+    const queryText = `SELECT * from review r inner join person p on p.id = r.user_id where r.place_id=${req.params.id};`
+    pool.query(queryText)
+    .then((results) => {
+        res.send(results.rows)
+    }).catch((err) => {
+        console.log('bad')
+        res.sendStatus(500)
+    })
+})
+
+/**
+ * POST route template
+ */
+router.post('/location', (req, res) => {
+const location = req.body;
+const queryText =`INSERT INTO place (name,address,city,longitude,latitude,vendor,user_id) 
+                VALUES($1,$2,$3,$4,$5,$6,$7);`
+pool.query(queryText, [location.name,location.address,
+    location.city,location.longitude,location.latitude,location.vendor, location.user_id])
+    .then((results) => {
+        res.sendStatus(201)
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+});
+
+router.post('/review', (req,res)=>{
+    const review = req.body
+    const queryText= `INSERT INTO review (user_id,place_id,comment) VALUES($1,$2,$3);`
+    pool.query(queryText, [ review.user_id, review.place_id, review.comment])
+    .then((results) => {
+        res.sendStatus(201)
+    }).catch((err) => {
+        res.sendStatus(500)
+    })
+})
+
+router.delete('/:id', (req,res)=>{
+    if (req.isAuthenticated()){
+        let id = req.params.id
+        let queryText = `DELETE from review WHERE review_id = $1;`
+        pool.query(queryText, [id] )
+        .then((results) => {
+            res.sendStatus(201)
+        }).catch((err) => {
+            res.sendStatus(500)
+        })
+
+    }
+
+})
+module.exports = router;
---
+++ pratice/server/routes/user.router.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const pool = require('../modules/pool');
+const userStrategy = require('../strategies/user.strategy');
+
+const router = express.Router();
+
+// Handles Ajax request for user information if user is authenticated
+router.get('/', rejectUnauthenticated, (req, res) => {
+  // Send back user object from database
+  res.send(req.user);
+});
+
+// Handles POST request with new user data
+// The only thing different from this and every other post we've seen
+// is that the password gets encrypted before being inserted
+router.post('/register', (req, res, next) => {
+  const username = req.body;
+  const password = encryptLib.encryptPassword(req.body.password);
+  const queryText = `INSERT INTO "person"
+   (username, password,firstname,lastname,city,user_type) 
+  VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`;
+  pool.query(queryText, [username.username, password,username.firstname,username.lastname,username.city,username.user_type])
+    .then(() => { res.sendStatus(201); })
+    .catch((err) => { next(err); });
+});
+
+// Handles login form authenticate/login POST
+// userStrategy.authenticate('local') is middleware that we run on this route
+// this middleware will run our POST if successful
+// this middleware will send a 404 if not successful
+router.post('/login', userStrategy.authenticate('local'), (req, res) => {
+  res.sendStatus(200);
+});
+
+// clear all server session information about this user
+router.get('/logout', (req, res) => {
+  // Use passport's built-in method to log out the user
+  req.logout();
+  res.sendStatus(200);
+});
+
+
+router.put('/:id',(req, res)=>{
+  updateUser = req.body
+  if(req.isAuthenticated()){
+    let queryText = `UPDATE person SET firstname =$2, lastname=$3, city=$4 WHERE id = $1;`;
+    pool.query(queryText, [req.user.id, updateUser.firstname, updateUser.lastname, updateUser.city])
+    .then(()=>{
+      res.sendStatus(200);
+    })
+    .catch((error)=>{
+      console.log('------error--------\n',error)
+      res.sendStatus(500);
+    })
+  }
+})
+
+module.exports = router;
