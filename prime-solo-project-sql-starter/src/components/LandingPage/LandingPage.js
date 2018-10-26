bab552df079c83aba3d19b1eec44d99712d7dc4f
Toua Thao <touathao09@gmail.com>
2018-10-26 13:56:03 -0500

    add line

--- prime-solo-project-sql-starter/src/components/LandingPage/LandingPage.js
+++ prime-solo-project-sql-starter/src/components/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ class LandingPage extends Component {
       return (
         <div>
        <h1>hello</h1>
-       <h1>hello</h1>
+       <h1>This is an annyoing website</h1>
         </div>
       );
     }
